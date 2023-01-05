1b8e1c5f45c7c024cd900f383d5a181c1673f9a7
Deepto <ddepp98@outlook.com>
2023-01-05 03:46:14 +0000

    Cleaned CSS, current state: red cube

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,18 @@ import { Canvas } from '@react-three/fiber'
 import { NextPage } from 'next'
 
 
-const Home: NextPage=()=> {
+const Home: NextPage = () => {
   return (
+    <div className='container'>
     <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial />
       </mesh>
     </Canvas>
+    </div>
   );
 };
 export default Home;
