69a05ff70c54bec23237980965fde4326d43cc8c
Deepto <ddepp98@outlook.com>
2023-01-05 17:40:33 +0000

    Added orbit controls. Current model : Rotatable cube

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 
-import { Canvas, ThreeElements } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { NextPage } from 'next'
 import AnimatedBox from '../components/AnimatedBox';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import { useEffect } from 'react';
 
+
+const CameraOrbitController = () => {
+  const {camera, gl} = useThree();
+  useEffect(()=>{
+    const controls = new OrbitControls(camera,gl.domElement)
+    return ()=>{
+      controls.dispose();
+    }
+  },[camera,gl]);
+
+  return null;
+}
 const Home: NextPage = () => {
   return (
     <div className='container'>
       <Canvas>
+        <CameraOrbitController/>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <AnimatedBox />
